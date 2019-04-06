13f1ff8bd6d1cf6458cae53baf7f0cf9846007c8
Vilius Vystartas <vilius.vystartas@gmail.com>
2019-04-06 12:12:54 +0100

    Connected the frontend and backend servers, changed "not-found" page font color, task #22 completed

--- src/backend/app.js
+++ src/backend/app.js
@@ -3,8 +3,15 @@ const shows = require('./shows');
 const app = express();
 const port = 5000;
 
-app.get('/api/shows', (req, res) => {
-    res.json(shows)
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get('/api/shows', (req, res, next) => {
+    res.json(shows);
+    next();
 });
 
 app.listen(port, () => console.log(`Backend app is listening on port ${port}!`))
