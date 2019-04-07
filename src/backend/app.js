7a944ca74e83ceb4927d9c8fcb37be97e1c435aa
Vilius Vystartas <vilius.vystartas@gmail.com>
2019-04-07 22:30:29 +0100

    Enabled the backend server in prod, task #23 completed, fixed header error

--- src/backend/app.js
+++ src/backend/app.js
@@ -12,7 +12,7 @@ app.use(function (req, res, next) {
 
 app.get('/api/shows', (req, res, next) => {
     res.json(shows);
-    next();
+    res.end();
 });
 
 app.use(express.static(path.join(__dirname, '../../build')));
