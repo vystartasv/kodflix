aa7e1ec9761e5b94fc9fa272d80126cc3a14114e
Vilius Vystartas <vilius.vystartas@gmail.com>
2019-04-10 09:37:50 +0100

    Enabled the backend server in prod, task #23 completed, fixed localhost error

--- src/backend/server.js
+++ src/backend/server.js
@@ -10,15 +10,17 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.get('/api/shows', (req, res, next) => {
+app.get('/api/shows', (req, res) => {
     res.json(shows);
     res.end();
 });
+if (process.env.NODE_ENV === 'prod') {
+    app.use(express.static(path.join(__dirname, '../../build')));
 
-app.use(express.static(path.join(__dirname, '../../build')));
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+    });
+}
 
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
-});
 
-app.listen(port, () => console.log(`Backend app is listening on port ${port}!`))
+app.listen(port, () => console.log(`Backend app running in '${process.env.NODE_ENV}' and is listening on port ${port}!`))
